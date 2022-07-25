973eca9b38a2e137ae1d347665e6c6b4751e8719
Thiago Bernardi <copocaneta@gmail.com>
2022-07-25 11:24:00 -0300

    feat: styling the Home component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,15 +1,16 @@
-import './index.scss';
 import LogoTitleT from '../../assets/images/t.svg';
 import LogoTitleB from '../../assets/images/b.svg';
 import { Link } from 'react-router-dom';
+import './index.scss';
 
 const Home = () => {
   return (
-    <div clasName="container home-page">
+    <div className="container home-page">
       <div className="text-zone">
         <h1>
-          Hi, <br /> I'm <img src={LogoTitleT} alt="Thiago" />
-          hiago <img src={LogoTitleB} alt="Bernardi" />
+          Hi, <br /> I'm{' '}
+          <img src={LogoTitleT} alt="Thiago" className="letter-t" />
+          hiago <img src={LogoTitleB} alt="Bernardi" className="letter-b" />
           ernardi
           <br />
           web developer
