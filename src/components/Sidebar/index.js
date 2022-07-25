5c7b7c0452096b683261b089baa23862e27bb9cf
Thiago Bernardi <copocaneta@gmail.com>
2022-07-25 03:12:16 -0300

    feat: sidebar section buttons and social media icons and links

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -2,6 +2,14 @@ import { Link, NavLink } from 'react-router-dom';
 import './index.scss';
 import LogoTb from '../../assets/images/logo-tb.png';
 import LogoSubtitle from '../../assets/images/logo-sub.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import {
+  faDiscord,
+  faGithub,
+  faLinkedin,
+  faStackOverflow,
+} from '@fortawesome/free-brands-svg-icons';
 
 const Sidebar = () => (
   <div className="nav-bar">
@@ -9,6 +17,65 @@ const Sidebar = () => (
       <img src={LogoTb} alt="logo" />
       <img className="sub-logo" src={LogoSubtitle} alt="Thiago Bernardi" />
     </Link>
+    <nav>
+      <NavLink exact="true" activeclassname="active" to="/">
+        <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+      </NavLink>
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="about-link"
+        to="/about"
+      >
+        <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+      </NavLink>
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="contact-link"
+        to="/contact"
+      >
+        <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+      </NavLink>
+    </nav>
+    <ul>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.linkedin.com/in/thiago-bernardi/"
+        >
+          <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/copocaneta/"
+        >
+          <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://stackoverflow.com/users/8754987/thiago-bernardi/"
+        >
+          <FontAwesomeIcon icon={faStackOverflow} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://discordapp.com/users/724435328823197757/"
+        >
+          <FontAwesomeIcon icon={faDiscord} color="#4d4d4e" />
+        </a>
+      </li>
+    </ul>
   </div>
 );
 
