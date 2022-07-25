16447eb016c840574b912b265ed3216799270ba7
Thiago Bernardi <copocaneta@gmail.com>
2022-07-25 02:43:41 -0300

    feat: adding logo and nav to the sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,5 +1,15 @@
+import { Link, NavLink } from 'react-router-dom';
 import './index.scss';
+import LogoTb from '../../assets/images/logo-tb.png';
+import LogoSubtitle from '../../assets/images/logo-sub.png';
 
-const Sidebar = () => <>Hello from sidebar</>;
+const Sidebar = () => (
+  <div className="nav-bar">
+    <Link className="logo" to="/">
+      <img src={LogoTb} alt="logo" />
+      <img className="sub-logo" src={LogoSubtitle} alt="Thiago Bernardi" />
+    </Link>
+  </div>
+);
 
 export default Sidebar;
