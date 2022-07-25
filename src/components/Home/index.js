0b0e731121bc955c400cf538b902fce29932fd10
Thiago Bernardi <copocaneta@gmail.com>
2022-07-25 12:17:34 -0300

    feat: adding arrays with name, lastname and job plus adding useEffect to change the state with classname after initial load

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,18 +2,64 @@ import LogoTitleT from '../../assets/images/t.svg';
 import LogoTitleB from '../../assets/images/b.svg';
 import { Link } from 'react-router-dom';
 import './index.scss';
+import { useEffect, useState } from 'react';
+import AnimatedLetters from '../AnimatedLetters';
 
 const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate');
+
+  const nameArray = ['h', 'i', 'a', 'g', 'o'];
+  const lastNameArray = ['e', 'r', 'n', 'a', 'r', 'd', 'i'];
+
+  const jobArray = [
+    'w',
+    'e',
+    'b',
+    ' ',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+  ];
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover');
+    }, 4000);
+  }, []);
+
   return (
     <div className="container home-page">
       <div className="text-zone">
         <h1>
-          Hi, <br /> I'm{' '}
-          <img src={LogoTitleT} alt="Thiago" className="letter-t" />
-          hiago <img src={LogoTitleB} alt="Bernardi" className="letter-b" />
-          ernardi
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i,</span>
           <br />
-          web developer
+          <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>'m</span>
+          <img src={LogoTitleT} alt="Thiago" className="letter-t" />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArr={nameArray}
+            idx={15}
+          />
+          <img src={LogoTitleB} alt="Bernardi" className="letter-b" />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArr={lastNameArray}
+            idx={20}
+          />
+          <br />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArr={jobArray}
+            idx={27}
+          />
         </h1>
         <h2>Fullstack JavaScript Developer</h2>
         <Link to="/contact" className="flat-button">
