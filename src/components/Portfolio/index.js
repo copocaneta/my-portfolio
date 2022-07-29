5122651f3d13fe2834f71f9107146c31e55d8142
Thiago Bernardi <copocaneta@gmail.com>
2022-07-29 11:05:21 -0300

    feat: adding portfolio section

---
+++ src/components/Portfolio/index.js
@@ -0,0 +1,24 @@
+import Loader from 'react-loaders';
+import './index.scss';
+import AnimatedLetters from '../AnimatedLetters';
+import { useState } from 'react';
+
+const Portfolio = () => {
+  const [letterClass, setLetterClass] = useState('text-animate');
+  return (
+    <>
+      <div className="container portfolio-page">
+        <h1 className="page-title">
+          <AnimatedLetters
+            strArr={'Portfolio'.split('')}
+            idx={15}
+            letterClass={letterClass}
+          />
+        </h1>
+      </div>
+      <Loader type="pacman" />
+    </>
+  );
+};
+
+export default Portfolio;
