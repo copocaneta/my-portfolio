b0b4d2ed2722e3da52464360c3e571f3246b63af
Thiago Bernardi <copocaneta@gmail.com>
2022-08-06 13:29:21 -0300

    refactor: fixing path for new dir structure

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import logo from './logo.svg';
 import './App.scss';
 import { Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
-import Portfolio from './components/Portfolio';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
+import Portfolio from './components/pages/Portfolio';
 
 function App() {
   return (
