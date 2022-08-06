dae326088c4877a65de7ecd7cc312fde07ebd51e
Thiago Bernardi <copocaneta@gmail.com>
2022-08-06 14:21:31 -0300

    refactor: changing home animation text

--- src/components/HomeHeadingText/index.js
+++ src/components/HomeHeadingText/index.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import LogoTitleT from '../../assets/images/t.svg';
 import LogoTitleB from '../../assets/images/b.svg';
 import AnimatedLetters from '../AnimatedLetters';
+import TypewriterComponent from 'typewriter-effect';
 
 const HomeHeadingText = () => {
   const [letterClass, setLetterClass] = useState('text-animate');
@@ -15,43 +16,37 @@ const HomeHeadingText = () => {
   }, []);
 
   return (
-    <Text
-      as="h1"
-      color={{ base: 'white', lg: theme.colors.textColorLight }}
-      textShadow={{ base: '2px 2px black;', lg: 'none' }}
-      wordBreak={'break-all'}
-      fontSize={{ base: '46px', lg: '53px' }}
-    >
-      <Text>
-        <span className={letterClass}>H</span>
-        <span className={`${letterClass} _12`}>i,</span>
-      </Text>
-      <Text>
-        <span className={`${letterClass} _13`}>I</span>
-        <span className={`${letterClass} _14`}>'m</span>
-      </Text>
-      <Text>
-        <img src={LogoTitleT} alt="Thiago" className="letter-t" />
-        <AnimatedLetters
-          letterClass={letterClass}
-          strArr={'hiago'.split('')}
-          idx={15}
+    <>
+      <Text
+        as="h1"
+        color={{ base: 'white', lg: theme.colors.textColorLight }}
+        textShadow={{ base: '2px 2px black;', lg: 'none' }}
+        wordBreak={'break-all'}
+        fontSize={{ base: '46px', lg: '53px' }}
+      >
+        <TypewriterComponent
+          onInit={(typewriter) => {
+            typewriter
+              .pauseFor(1000)
+              .typeString('Hello, I am Thiago Bernardi!')
+              .pauseFor(2500)
+              .deleteChars(1)
+              .typeString(', <br> a Frontend')
+              .deleteChars(8)
+              .typeString('Backend')
+              .deleteChars(7)
+              .typeString('Fullstack web developer')
+
+              .start();
+          }}
+          // options={{
+          //   strings: ['Hello', 'I am Thiago Bernardi'],
+          //   autoStart: true,
+          //   loop: true,
+          // }}
         />
-        <img src={LogoTitleB} alt="Bernardi" className="letter-b" />
-        <AnimatedLetters
-          letterClass={letterClass}
-          strArr={'ernardi'.split('')}
-          idx={20}
-        />
       </Text>
-      {/* <Text>
-              <AnimatedLetters
-                letterClass={letterClass}
-                strArr={'web developer'.split('')}
-                idx={27}
-              />
-            </Text> */}
-    </Text>
+    </>
   );
 };
 
