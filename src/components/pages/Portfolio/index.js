b595addc697a8ae346297ff5022cab5ac9e7e852
Thiago Bernardi <copocaneta@gmail.com>
2022-08-07 03:23:57 -0300

    refactor: making portfolio page responsive

--- src/components/pages/Portfolio/index.js
+++ src/components/pages/Portfolio/index.js
@@ -3,22 +3,37 @@ import './index.scss';
 import AnimatedLetters from '../../AnimatedLetters';
 import { useState } from 'react';
 import PageLoader from '../../PageLoader';
+import { Box, Grid, GridItem } from '@chakra-ui/react';
+import { motion } from 'framer-motion';
+import { pageVariants } from '../../../utils/page-transition';
 
 const Portfolio = () => {
   const [letterClass, setLetterClass] = useState('text-animate');
   return (
-    <>
-      <div className="container portfolio-page">
-        <h1 className="page-title">
-          <AnimatedLetters
-            strArr={'Portfolio'.split('')}
-            idx={15}
-            letterClass={letterClass}
-          />
-        </h1>
-      </div>
+    <Box display={'flex'} justifyContent={'center'} w={'100%'} h={'100%'}>
+      <Grid
+        as={motion.div}
+        variants={pageVariants}
+        initial="initial"
+        animate="animate"
+        exit="out"
+        templateColumns={'1fr 1fr'}
+        w={'100%'}
+        mx={'50px'}
+        className="container portfolio-page"
+      >
+        <GridItem>
+          <h1 className="page-title">
+            <AnimatedLetters
+              strArr={'Portfolio'.split('')}
+              idx={15}
+              letterClass={letterClass}
+            />
+          </h1>
+        </GridItem>
+      </Grid>
       <PageLoader />
-    </>
+    </Box>
   );
 };
 
