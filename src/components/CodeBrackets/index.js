008b2b9475e84b7c1c0a35563d166e328ce3777e
Thiago Bernardi <copocaneta@gmail.com>
2022-08-27 21:01:56 -0300

    refactor: adding ether icon to floating elements and improving animation

--- src/components/CodeBrackets/index.js
+++ src/components/CodeBrackets/index.js
@@ -43,8 +43,8 @@ const CodeBrackets = () => {
       {/* js */}
       <svg
         class="svg"
-        width="240px"
-        height="240px"
+        width="130px"
+        height="130px"
         viewBox="0 0 24 24"
         xmlns="http://www.w3.org/2000/svg"
       >
@@ -82,8 +82,8 @@ const CodeBrackets = () => {
       {/* typescript */}
       <svg
         class="svg"
-        width="240px"
-        height="240px"
+        width="110px"
+        height="110px"
         viewBox="0 0 256 256"
         version="1.1"
         xmlns="http://www.w3.org/2000/svg"
@@ -105,6 +105,30 @@ const CodeBrackets = () => {
             fill="#FFFFFF"
             transform="translate(194.578507, 176.190240) scale(1, -1) translate(-194.578507, -176.190240) "
           ></path>
+        </g>
+      </svg>
+      {/* ether */}
+
+      <svg
+        class="svg"
+        width="80px"
+        height="80px"
+        viewBox="0 0 32 32"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g fill="none" fill-rule="evenodd">
+          <circle cx="16" cy="16" r="16" fill="#627EEA" />
+          <g fill="#FFF" fill-rule="nonzero">
+            <path fill-opacity=".602" d="M16.498 4v8.87l7.497 3.35z" />
+            <path d="M16.498 4L9 16.22l7.498-3.35z" />
+            <path fill-opacity=".602" d="M16.498 21.968v6.027L24 17.616z" />
+            <path d="M16.498 27.995v-6.028L9 17.616z" />
+            <path
+              fill-opacity=".2"
+              d="M16.498 20.573l7.497-4.353-7.497-3.348z"
+            />
+            <path fill-opacity=".602" d="M9 16.22l7.498 4.353v-7.701z" />
+          </g>
         </g>
       </svg>
     </Box>
