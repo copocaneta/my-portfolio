85c611e45bf1eefa46dd17887bcd86c019e55462
Thiago Bernardi <copocaneta@gmail.com>
2022-09-07 22:11:00 -0300

    refactor: renaming Sidebar to menu

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,20 +1,20 @@
-import { Box, Container, Grid, GridItem, Text } from '@chakra-ui/react';
+import { Grid, GridItem } from '@chakra-ui/react';
 import { Outlet } from 'react-router-dom';
-import Sidebar from '../Sidebar';
+import Sidebar from '../Menu';
 import './index.scss';
 
 const Layout = () => {
   return (
     <Grid
       templateColumns={{ base: '1fr', lg: '68px 1fr' }}
-      templateRows={{ base: '100px 1fr', lg: 'auto' }}
+      templateRows={{ base: '90px 1fr', lg: 'auto' }}
       maxW={'100%'}
       width={'100%'}
       height={'100%'}
     >
       {/* <div className="App"> */}
-      <Sidebar />
-      <GridItem className="page" marginTop={{ base: '30px', lg: 'unset' }}>
+      <Sidebar id={'navbar-menu'} />
+      <GridItem className="page">
         <Outlet />
       </GridItem>
       {/* </div> */}
