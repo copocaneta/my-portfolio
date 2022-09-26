b50cc650e35c37d2b96f464ead5a1a6dac147ef2
Thiago Bernardi <copocaneta@gmail.com>
2022-09-25 22:14:16 -0300

    refactor: removing contact form from Contact page and adding Telegram and Discord buttons

--- src/components/pages/Contact/index.js
+++ src/components/pages/Contact/index.js
@@ -1,65 +1,31 @@
 import { useEffect, useRef, useState } from 'react';
 import AnimatedLetters from '../../AnimatedLetters';
-import emailjs from '@emailjs/browser';
 import './index.scss';
-import ReCAPTCHA from 'react-google-recaptcha';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import PageLoader from '../../PageLoader';
-import { Box, Grid, GridItem, useTheme } from '@chakra-ui/react';
+import {
+  Box,
+  Flex,
+  Grid,
+  GridItem,
+  IconButton,
+  Link,
+  Text,
+  useTheme,
+} from '@chakra-ui/react';
 import { motion } from 'framer-motion';
 import { pageVariants } from '../../../utils/page-transition';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDiscord, faTelegram } from '@fortawesome/free-brands-svg-icons';
 
 const Contact = () => {
   const [letterClass, setLetterClass] = useState('text-animate');
-
-  const refForm = useRef();
-
-  const refCaptcha = useRef();
 
   useEffect(() => {
     setTimeout(() => {
       setLetterClass('text-animate-hover');
     }, 3000);
   }, []);
-
-  const [open, setOpen] = useState(false);
-
-  const [toSend, setToSend] = useState({
-    name: '',
-    email: '',
-    subject: '',
-    message: '',
-  });
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const token = refCaptcha.current.getValue();
-    console.log('token', token);
-    const params = {
-      ...toSend,
-      'g-recaptcha-response': token,
-    };
-    emailjs
-      .send(
-        process.env.REACT_APP_EMAIL_JS_SERVICE,
-        process.env.REACT_APP_EMAIL_JS_TEMPLATE,
-        params,
-        process.env.REACT_APP_EMAIL_JS_USER
-        // `g-recaptcha-response: ${}`
-      )
-      .then(
-        ({ status }) => {
-          console.log('EMAILJS SENT', status.code);
-        },
-        (err) => {
-          console.log('EMAILJS ERROR', err);
-        }
-      );
-  };
-
-  const handleChange = (e) => {
-    setToSend({ ...toSend, [e.target.name]: e.target.value });
-  };
 
   const theme = useTheme();
 
@@ -97,63 +63,44 @@ const Contact = () => {
               idx={15}
             />
           </h1>
-          <p>
-            If you would like to get in touch with me, please do not hesitate to
-            contact me using the form below.
-          </p>
-          <div className="contact-form">
-            <form ref={refForm} onSubmit={handleSubmit}>
-              <ul>
-                <li className="half">
-                  <input
-                    type="text"
-                    name="name"
-                    value={open.name}
-                    onChange={handleChange}
-                    placeholder="Name"
-                    required
+          <Text>
+            If you would like to get in touch with me, use one of the options
+            below.
+          </Text>
+          <Flex marginTop={'40px'} gap={'20px'}>
+            <Link
+              target="_blank"
+              rel="noreferrer"
+              href="https://t.me/thiago_bernardi"
+              isExternal
+            >
+              <IconButton
+                icon={
+                  <FontAwesomeIcon
+                    icon={faTelegram}
+                    size={'3x'}
+                    color={'#229ED9'}
                   />
-                </li>
-                <li className="half">
-                  <input
-                    type="email"
-                    name="email"
-                    value={open.email}
-                    onChange={handleChange}
-                    placeholder="E-mail"
-                    required
-                  />
-                </li>
-                <li>
-                  <input
-                    value={open.subject}
-                    onChange={handleChange}
-                    placeholder="Subject"
-                    type="text"
-                    name="subject"
-                    required
-                  />
-                </li>
-                <li>
-                  <textarea
-                    value={open.message}
-                    onChange={handleChange}
-                    placeholder="Message"
-                    name="message"
-                    required
-                  ></textarea>
-                </li>
-                <li>
-                  <input type="submit" className="flat-button" value="SEND" />
-                </li>
-              </ul>
-              <ReCAPTCHA
-                ref={refCaptcha}
-                sitekey={process.env.REACT_APP_SITE_KEY}
-                // onChange={testCaptcha}
+                }
               />
-            </form>
-          </div>
+            </Link>
+            <Link
+              target="_blank"
+              rel="noreferrer"
+              href="https://discordapp.com/users/724435328823197757/"
+              isExternal
+            >
+              <IconButton
+                icon={
+                  <FontAwesomeIcon
+                    icon={faDiscord}
+                    color="#7289da"
+                    size={'3x'}
+                  />
+                }
+              />
+            </Link>
+          </Flex>
         </GridItem>
         <GridItem>
           <Box
