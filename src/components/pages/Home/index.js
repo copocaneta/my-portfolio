00b92c7b1b9a353f8fe3a6e853427a4d6649981d
Thiago Bernardi <copocaneta@gmail.com>
2023-06-18 16:02:35 -0300

    refactor: refactoring home to add new 3d canvas

--- src/components/pages/Home/index.js
+++ src/components/pages/Home/index.js
@@ -1,6 +1,14 @@
 import { Link } from 'react-router-dom';
 import './index.scss';
-import { Box, Button, Grid, GridItem, Text, useTheme } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Flex,
+  Grid,
+  GridItem,
+  Text,
+  useTheme,
+} from '@chakra-ui/react';
 import HomeHeadingText from '../../HomeHeadingText';
 import HomePicture from '../../HomePicture';
 import CodeBrackets from '../../CodeBrackets';
@@ -8,95 +16,79 @@ import PageLoader from '../../PageLoader';
 import { motion } from 'framer-motion';
 import { pageVariants } from '../../../utils/page-transition';
 import mycv from '../../../assets/cv/resume_thiago_bernardi.pdf';
+import HomeCanvas from '../../canvas/Home';
+import { useEffect, useRef, useState } from 'react';
+
+// const useDelayedRender = (delay) => {
+//   const [delayed, setDelayed] = useState(true);
+//   useEffect(() => {
+//     const timeout = setTimeout(() => setDelayed(false), delay);
+//     return () => clearTimeout(timeout);
+//   }, []);
+//   return (fn) => !delayed && fn();
+// };
 
 const Home = () => {
   const theme = useTheme();
+  const stageCanvasRef = useRef(null);
+  // const delayedRender = useDelayedRender(15500);
 
   return (
     <Box display={'flex'} justifyContent={'center'} w={'100%'} h={'100%'}>
-      <Grid
+      <Box
         as={motion.div}
         variants={pageVariants}
         initial="initial"
         animate="animate"
         exit="out"
         className="container home-page"
-        templateColumns={{ base: '1fr', md: '1fr 1fr' }}
         w={'100%'}
         mx={{ base: '20px', lg: '50px' }}
       >
-        <GridItem
-          className="text-zone"
-          display={'flex'}
-          flexDirection={'column'}
-          justifyContent={'center'}
-          zIndex={999999}
-        >
-          <HomeHeadingText />
-
-          <Text
-            marginTop={'10px'}
-            as="h2"
-            display={{ base: 'none', lg: 'block' }}
-          >
-            Fullstack JavaScript Developer
-          </Text>
-
-          <Box
-            display={'flex'}
-            gap={'10px'}
-            marginTop={'25px'}
-            flexDir={{ base: 'column', lg: 'row' }}
-          >
-            <Button
-              as={Link}
-              to="/contact"
-              className="regular-button"
-              border={'none'}
-              p={'18px 18px'}
-              background={theme.colors.altColorLight}
-              color={'white'}
-              _hover={{
-                color: 'white',
-                textDecoration: 'none',
-                background: '#BE6022;',
-              }}
-            >
-              CONTACT ME
-            </Button>
-            <Button
-              as={'a'}
-              download={'My Resume'}
-              href={mycv}
-              cursor={'pointer'}
-              className="regular-button"
-              border={'none'}
-              p={'18px 18px'}
-              background={theme.colors.altColorLight}
-              color={'white'}
-              _hover={{
-                color: 'white',
-                textDecoration: 'none',
-                background: '#BE6022;',
-              }}
-            >
-              DOWNLOAD CV
-            </Button>
-          </Box>
-        </GridItem>
-        <GridItem
+        <Flex
           zIndex={999998}
-          position={{ base: 'absolute', md: 'unset' }}
+          flex={1}
+          // position={{ base: 'absolute', md: 'unset' }}
           display={'flex'}
           width={'100%'}
           height={{ base: '100%' }}
           justifyContent={{ base: 'center', md: 'unset' }}
-          className={'image-zone'}
+          // className={'image-zone'}
         >
-          <CodeBrackets />
-          <HomePicture />
-        </GridItem>
-      </Grid>
+          {/* <Text>Teste</Text> */}
+          <Flex position="absolute">
+            <Text
+              as="h1"
+              color={{ base: 'white', lg: theme.colors.textColorLight }}
+              textShadow={{ base: '2px 2px black;', lg: 'none' }}
+              wordBreak={'break-word'}
+              fontSize={{ base: '46px', lg: '53px' }}
+              lineHeight={'38px'}
+              // className={'home-heading'}
+            >
+              Hi, I'm Thiago Bernardi
+            </Text>
+          </Flex>
+          {/* <Flex */}
+          {/*   flex={1} */}
+          {/*   justify="center" */}
+          {/*   alignItems="center" */}
+          {/*   width="100%" */}
+          {/*   height="100%" */}
+          {/*   position="relative" */}
+          {/*   mx="auto" */}
+          {/* > */}
+          <>
+            <Flex flex={1} ref={stageCanvasRef}>
+              <HomeCanvas parentRef={stageCanvasRef} />
+            </Flex>
+            {/* useDelayedRender( */}
+            {/* <HomeCanvas />) */}
+          </>
+          {/* <Text>Teste</Text> */}
+          {/* </Flex> */}
+        </Flex>
+      </Box>
       <PageLoader />
     </Box>
   );
