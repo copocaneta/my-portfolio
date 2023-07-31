c7f72f97a6003d0c668d775f298d3193537c0dbc
Thiago Bernardi <copocaneta@gmail.com>
2023-07-31 02:50:53 -0300

    refactor: adding index with imports and exports asset stuff

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,6 @@
 import crowdform from './images/about/crowdform.png';
 import metabravo from './images/about/metabravo.png';
+import github from './images/github.png';
+import equalizer from './images/portffolio/equalizer.jpeg';
 
-export { crowdform, metabravo };
+export { github, crowdform, metabravo, equalizer };
