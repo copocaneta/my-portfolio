cfdab3983272ec1ac99e19799efeaa3c7f1839a5
Thiago Bernardi <copocaneta@gmail.com>
2025-01-01 17:49:55 -0300

    chore: cleaning up

--- src/assets/index.js
+++
@@ -1,6 +0,0 @@
-import crowdform from './images/about/crowdform.png';
-import metabravo from './images/about/metabravo.png';
-import github from './images/github.png';
-import equalizer from './images/portffolio/equalizer.jpeg';
-
-export { github, crowdform, metabravo, equalizer };
